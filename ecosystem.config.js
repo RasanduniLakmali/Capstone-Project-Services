755ce914ccc515fbbc2dfcee58764d6dcbd60c0e
RasanduniLakmali <rasandunilakmali26@gmail.com>
2026-03-31 06:56:37 +0530

    Updated ecosystem.config.js fle

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+      apps : [
+            {
+              name   : "student-service",
+              script : "java -jar student-service/target/Student-Service-1.0.0.jar",
+              log_files: "./logs/student-service.logs"
+            },
+            {
+              name   : "program-service",
+              script : "java -jar program-service/target/Program-Service-1.0.0.jar",
+                log_files: "./logs/program-service.logs"
+            },
+            {
+              name   : "enrollment-service",
+              script : ".java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
+                log_files: "./logs/enrollment-service.logs"
+            }
+      ]
+}
