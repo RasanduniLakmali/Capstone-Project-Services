9a6078bfd075c9ac8cd5f45bca43263c4b408577
RasanduniLakmali <rasandunilakmali26@gmail.com>
2026-03-31 13:30:36 +0530

    Updated ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,20 @@ module.exports = {
             {
               name   : "student-service",
               script : "java -jar student-service/target/Student-Service-1.0.0.jar",
-              log_files: "./logs/student-service.logs"
+              log_files: "./logs/student-service.logs",
+              instances :   2
             },
             {
               name   : "program-service",
               script : "java -jar program-service/target/Program-Service-1.0.0.jar",
-                log_files: "./logs/program-service.logs"
+                log_files: "./logs/program-service.logs",
+                instances :   2
             },
             {
               name   : "enrollment-service",
               script : ".java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
-                log_files: "./logs/enrollment-service.logs"
+                log_files: "./logs/enrollment-service.logs",
+                instances :   2
             }
       ]
 }
