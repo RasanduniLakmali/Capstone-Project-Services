6f2e6b7a4fc00b8cdf691279bfc2212602f0d032
RasanduniLakmali <rasandunilakmali26@gmail.com>
2026-03-31 21:30:48 +0530

    Add cloud sql auth proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,26 @@
 module.exports = {
       apps : [
+          {
+             name : "cloud-sql-auth-proxy",
+             script: "./cloud-sql-proxy project-219b5172-0644-4a7f-87b:asia-southeast1:mysql-vm project-219b5172-0644-4a7f-87b:asia-southeast1:postgres-vm --private-ip",
+              log_files: "./logs/cloud-sql-proxy.log",
+          },
             {
               name   : "student-service",
               script : "java -jar student-service/target/Student-Service-1.0.0.jar",
-              log_files: "./logs/student-service.logs",
+              log_files: "./logs/student-service.log",
               instances :   2
             },
             {
               name   : "program-service",
               script : "java -jar program-service/target/Program-Service-1.0.0.jar",
-                log_files: "./logs/program-service.logs",
+                log_files: "./logs/program-service.log",
                 instances :   2
             },
             {
               name   : "enrollment-service",
               script : ".java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
-                log_files: "./logs/enrollment-service.logs",
+                log_files: "./logs/enrollment-service.log",
                 instances :   2
             }
       ]
